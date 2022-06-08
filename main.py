5593df1efd9ddb62c6dd6109ff4514a17fff2938
febriajim <ajifebri5@gmail.com>
2022-06-08 14:16:03 +0700

    first

---
+++ main.py
@@ -0,0 +1,139 @@
+import os
+import cv2
+import math
+import pickle
+import numpy as np
+import face_recognition
+from sklearn import neighbors
+from PIL import Image, ImageDraw
+from face_recognition.face_recognition_cli import image_files_in_folder
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
+    X = []
+    y = []
+
+    for class_dir in os.listdir(train_dir):
+        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+            continue
+
+        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+            image = face_recognition.load_image_file(img_path)
+            face_bounding_boxes = face_recognition.face_locations(image)
+
+            if len(face_bounding_boxes) != 1:
+                if verbose:
+                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+            else:
+                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+                y.append(class_dir)
+
+    if n_neighbors is None:
+        n_neighbors = int(round(math.sqrt(len(X))))
+        if verbose:
+            print("Chose n_neighbors automatically:", n_neighbors)
+
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+    knn_clf.fit(X, y)
+
+    if model_save_path is not None:
+        with open(model_save_path, 'wb') as f:
+            pickle.dump(knn_clf, f)
+
+    return knn_clf
+
+def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
+    # if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+    #     raise Exception("Invalid image path: {}".format(X_img_path))
+
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
+
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    X_img = face_recognition.load_image_file(X_img_path)
+    X_face_locations = face_recognition.face_locations(X_img)
+
+    if len(X_face_locations) == 0:
+        return []
+
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+def show_prediction_labels_on_image(img_path, predictions):
+    pil_image = Image.open(img_path).convert("RGB")
+    draw = ImageDraw.Draw(pil_image)
+
+    for name, (top, right, bottom, left) in predictions:
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        name = name.encode("UTF-8")
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+    del draw
+    pil_image.show()
+
+def realtime():
+    video_capture = cv2.VideoCapture(0)
+    process_this_frame = True
+
+    while True:
+        ret, frame = video_capture.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        if process_this_frame:
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            if len(face_locations) != 1:
+                continue
+
+            print("Looking for faces in frame")
+            predictions = predict(rgb_small_frame, model_path="model.clf")
+            print(predictions)
+
+        process_this_frame = not process_this_frame
+
+        for name, (top, right, bottom, left) in predictions:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            print("- Found {} at ({}, {})".format(name, left, top))
+
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+def main():
+    print("Training KNN classifier...")
+    classifier = train("dataset", model_save_path="model.clf", n_neighbors=2)
+    print("Training complete!")
+
+    for image_file in os.listdir("dataset_test"):
+        full_file_path = os.path.join("dataset_test", image_file)
+
+        print("Looking for faces in {}".format(image_file))
+        predictions = predict(full_file_path, model_path="model.clf")
+
+        for name, (top, right, bottom, left) in predictions:
+            print("- Found {} at ({}, {})".format(name, left, top))
+
+        # show_prediction_labels_on_image(os.path.join("dataset_test", image_file), predictions)
+
+realtime()
